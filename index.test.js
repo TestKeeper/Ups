8afc8c7e0787018c3ae36c310d308b22951294c6
TestKeeper <97809269+TestKeeper@users.noreply.github.com>
2025-05-17 04:15:35 +0800

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,11 +1,11 @@
 const { test, expect } = require('@playwright/test');
 
-test('basic test', async ({ page }) => {
+test('Test on Site A', async ({ page }) => {
   await page.goto('https://app.upscale.stormtrade.dev/sign-in');
-  await page.waitForLoadState('domcontentloaded');
+  await expect(page).toHaveTitle(/Upscale — Prop Trading in Telegram • Capital up to $100 000/);
+});
 
-  const title = await page.title();
-  console.log('TITLE:', title);
-
-  await expect(page).toHaveTitle('Upscale — Prop Trading in Telegram • Capital up to $100 000');
+test('Test on Site B', async ({ page }) => {
+  await page.goto('https://app.upscale.trade/sign-in');
+  await expect(page).toHaveTitle(/Upscale — Prop Trading in Telegram • Capital up to $100 000/);
 });
