6dbe8f52d7847056fd99c69581cb5c97b6f9c944
TestKeeper <97809269+TestKeeper@users.noreply.github.com>
2025-05-17 14:12:38 +0800

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+// ✅ Функция отправки сообщения в Telegram
+async function sendTelegramMessage(message) {
+  const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
+  const TG_CHAT_ID = process.env.TG_CHAT_ID;
+
   await fetch(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -6,21 +13,24 @@
       text: message,
     }),
   });
-};
+}
 
+// ✅ Ожидаемый заголовок страницы
 const expectedTitle = 'Upscale — Prop Trading in Telegram • Capital up to $100 000';
 
+// ✅ Тест стейдж-сервера
 test('Test on Site A', async ({ page }) => {
   try {
     await page.goto('https://app.upscale.stormtrade.dev/sign-in');
     await expect(page).toHaveTitle(expectedTitle);
     await sendTelegramMessage('✅ Стейдж сервер доступен!');
   } catch (e) {
-    await sendTelegramMessage(`❌ СТейдж сервер упал: ${e.message}`);
+    await sendTelegramMessage(`❌ Стейдж сервер упал: ${e.message}`);
     throw e;
   }
 });
 
+// ✅ Тест прод-сервера
 test('Test on Site B', async ({ page }) => {
   try {
     await page.goto('https://app.upscale.trade/sign-in');
@@ -31,3 +41,4 @@ test('Test on Site B', async ({ page }) => {
     throw e;
   }
 });
+
