5b362aec853177d44c087922ed283edf2a791828
TestKeeper <97809269+TestKeeper@users.noreply.github.com>
2025-05-17 13:52:58 +0800

    Create tests/index.test.js

---
+++ tests/index.test.js
@@ -0,0 +1,33 @@
+  await fetch(`https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      chat_id: TG_CHAT_ID,
+      text: message,
+    }),
+  });
+};
+
+const expectedTitle = 'Upscale — Prop Trading in Telegram • Capital up to $100 000';
+
+test('Test on Site A', async ({ page }) => {
+  try {
+    await page.goto('https://app.upscale.stormtrade.dev/sign-in');
+    await expect(page).toHaveTitle(expectedTitle);
+    await sendTelegramMessage('✅ Стейдж сервер доступен!');
+  } catch (e) {
+    await sendTelegramMessage(`❌ СТейдж сервер упал: ${e.message}`);
+    throw e;
+  }
+});
+
+test('Test on Site B', async ({ page }) => {
+  try {
+    await page.goto('https://app.upscale.trade/sign-in');
+    await expect(page).toHaveTitle(expectedTitle);
+    await sendTelegramMessage('✅ Прод сервер доступен');
+  } catch (e) {
+    await sendTelegramMessage(`❌ Прод сервер упал: ${e.message}`);
+    throw e;
+  }
+});
