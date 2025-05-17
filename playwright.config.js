20f50ceb539fd1018df1c9e42f7e359ce8a9fe56
TestKeeper <97809269+TestKeeper@users.noreply.github.com>
2025-05-17 14:00:57 +0800

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,10 @@
 // playwright.config.js
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  testDir: './tests', // путь к директории с тестами
   use: {
-    headless: true,  // чтобы браузер был виден
+    headless: true, // запускаем браузер в headless режиме
   },
 };
+
 module.exports = config;
